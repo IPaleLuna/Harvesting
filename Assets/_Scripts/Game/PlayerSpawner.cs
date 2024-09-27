c344b9caf277a37f4c96dbb3a608958c9a8d96c5
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-09-27 15:25:31 +0300

    Спавн игроков

---
+++ Assets/_Scripts/Game/PlayerSpawner.cs
@@ -0,0 +1,51 @@
+using Cysharp.Threading.Tasks;
+using PaleLuna.Architecture.Initializer;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpawner : MonoBehaviour, IInitializer
+{
+    private InitStatus _status = InitStatus.Shutdown;
+    public InitStatus status => _status;
+
+    private string _playerPrefabsPath = "Prefabs/Players/Player";
+    private int _playerCount = 0;
+
+    public void StartInit()
+    {
+        _status = InitStatus.Initialization;
+
+        _playerCount = PlayerPrefs.GetInt(PlayerPrefsKeys.PLAYER_COUNT_KEY);
+
+        _ = InitPlayers();
+    }
+
+    private async UniTaskVoid InitPlayers()
+    {
+        for(int i = 0; i < _playerCount; i++)
+        {
+            string playerConcretePath = _playerPrefabsPath + $" {i+1}";
+
+            ResourceRequest request = Resources.LoadAsync<GameObject>(playerConcretePath);
+
+            await request;
+
+            if(request.asset == null)
+                throw new ArgumentNullException($"path: {playerConcretePath} dont contain GameObject");
+
+            CreatePlayer(request.asset as GameObject);
+        }
+
+        _status = InitStatus.Done;
+    }
+
+    private void CreatePlayer(GameObject playerPrefab)
+    {
+        PlayerInput playerInput = Instantiate(playerPrefab).GetComponentInChildren<PlayerInput>();
+
+        if (playerInput.playerIndex < 2)
+            playerInput.SwitchCurrentControlScheme(playerInput.currentControlScheme, Keyboard.current);
+    }
+
+}
