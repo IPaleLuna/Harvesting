f8580d7c9c0880971b0ebb5f173becb3f8214cd2
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-03 12:12:05 +0300

    Update GameEvents.cs

--- Assets/_Scripts/Game/Gameplay/GameEvents.cs
+++ Assets/_Scripts/Game/Gameplay/GameEvents.cs
@@ -4,4 +4,6 @@ using UnityEngine.Events;
 public class GameEvents : MonoBehaviour
 {
     public static readonly UnityEvent<Player> applePickEvent = new();
+
+    public static readonly UnityEvent timeBroadcasterInitEvent = new();
 }
