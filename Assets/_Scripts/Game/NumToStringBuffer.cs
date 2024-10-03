c3b9b9a59be4b8d9ce7928a2a7940b263abc2ff3
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-03 12:11:13 +0300

    Реализация буфера с числами

---
+++ Assets/_Scripts/Game/NumToStringBuffer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
+
+public static class NumToStringBuffer
+{
+    private const int START_CAPACITY = 100;
+
+    private static List<string> _numbersBuffer;
+
+    public static string GetIntToStringHash(int num)
+    {
+        if (_numbersBuffer == null) InitList();
+        if (num >= _numbersBuffer.Count) ResizeBuffer(num);
+
+        return _numbersBuffer[num];
+    }
+
+
+    private static void InitList()
+    {
+        _numbersBuffer = new List<string>(START_CAPACITY);
+        ResizeBuffer(START_CAPACITY);
+    }
+    private static void ResizeBuffer(int newCapacity)
+    {
+        for (int i = _numbersBuffer.Count; i < newCapacity; i++)
+            _numbersBuffer.Add(i.ToString());
+    }
+}
