52d9fbdb8314657aa451a0ef72c4265dfd2ee9ef
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-05 18:19:25 +0300

    Логика для UI в конце игры

---
+++ Assets/_Scripts/UIScripts/Game/GameEndScreen.cs
@@ -0,0 +1,28 @@
+using Services;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class GameEndScreen : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _winText;
+    [SerializeField]
+    private GameObject _hitGo;
+
+
+    private void Start()
+    {
+        GameEvents.timeOutEvent.AddListener(OnTimeOut);
+    }
+
+    private void OnTimeOut()
+    {
+        Player winPlayer = ServiceManager.Instance.SceneLocator.Get<ScoreHolder>().playerWithMaxScore;
+
+        _winText.SetText($"Player {winPlayer.playerID + 1} win!");
+
+        _winText.gameObject.SetActive(true);
+        _hitGo.SetActive(true);
+    }
+}
