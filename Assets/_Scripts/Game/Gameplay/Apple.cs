efea5fcf7fd057bd3b8a64a81b3abbe0511bc07e
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-18 11:21:59 +0300

    Update Apple.cs

--- Assets/_Scripts/Game/Gameplay/Apple.cs
+++ Assets/_Scripts/Game/Gameplay/Apple.cs
@@ -1,18 +1,27 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    [Header("Type of apple"), HorizontalLine(color: EColor.Red)]
+    [SerializeField]
+    private AppleType _type;
+
+    [Header("Apple properties"), HorizontalLine(color: EColor.Green)]
     [SerializeField]
     private int _cost = 1;
 
+    public AppleType type => _type;
     public int cost => _cost;
 
     public void PickThis()
     {
         gameObject.SetActive(false);
+        GameEvents.appleWasPicked.Invoke(this);
     }
-    public void RespawnThis()
+    public void RespawnThis(Vector2 pos)
     {
-
+        transform.position = pos;
+        gameObject.SetActive(true);
     }
 }
