7ee4025506d7a5f4c328d9402cddf60ea7c6e986
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-24 18:15:09 +0300

    Update GameEvents.cs
    Новое событие

--- Assets/_Scripts/Game/Gameplay/GameEvents.cs
+++ Assets/_Scripts/Game/Gameplay/GameEvents.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 public class GameEvents : MonoBehaviour
 {
     public static readonly UnityEvent<Player> playerPickApple = new();
-    public static readonly UnityEvent<Apple> appleWasPicked = new();
+    public static readonly UnityEvent<Apple> appleWasDeactivated = new();
 
     public static readonly UnityEvent timeOutEvent = new();
 }
