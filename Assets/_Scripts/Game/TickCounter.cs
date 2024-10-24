2a31f9b2d15c13e430d1af485be2c3671bf85f6e
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-24 18:16:24 +0300

    Update TickCounter.cs

--- Assets/_Scripts/Game/TickCounter.cs
+++ Assets/_Scripts/Game/TickCounter.cs
@@ -5,16 +5,16 @@ using Services;
 using System;
 using UnityEngine;
 using UnityEngine.Events;
-using Random = UnityEngine.Random;
 
 [Serializable]
-public class TickCounter : ITickUpdatable
+public class TickCounter : ITickUpdatable, IPausable
 {
     private GameLoops _gameLoops;
 
     [SerializeField, Range(0, 60)]
     private int _targetTicks;
 
+    [SerializeField, ReadOnly]
     private int _currentTicks = 0;
 
     private UnityAction _callbackAction;
@@ -24,9 +24,33 @@ public class TickCounter : ITickUpdatable
         _callbackAction = callback;
     }
 
+    public void SetUp(UnityAction callback)
+    {
+        SetCallback(callback);
+        _gameLoops = ServiceManager.Instance.GlobalServices.Get<GameLoops>();
+
+        _gameLoops.pausablesHolder.Registration(this);
+    }
+
+    public void SetTarget(int target)
+    {
+        _targetTicks = target;
+        ResetCounter();
+    }
+
+    public void OnPause()
+    {
+        _gameLoops?.Unregistration(this);
+    }
+
+    public void OnResume()
+    {
+        _gameLoops?.Registration(this);
+    }
+
     public void EveryTickRun()
     {
-        _currentTicks++;
+        _currentTicks += 1;
         CheckCounter();
     }
 
@@ -38,8 +62,13 @@ public class TickCounter : ITickUpdatable
         _callbackAction?.Invoke();
     }
 
-    private void ResetCounter()
-    {
+    private void ResetCounter() =>
         _currentTicks = 0;
+
+    ~TickCounter()
+    {
+        OnPause();
+        _gameLoops.pausablesHolder.Unregistration(this);
     }
+    
 }
