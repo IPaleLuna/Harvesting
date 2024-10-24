b8d5c884b99acb542863b302406678fe7dff8e0a
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-24 18:13:38 +0300

    Переработка яблок
    Полная переработка класса.
    Характеристики вынесены в отдельный SO
    Реализована механика гниения. Теперь яблоко портится со временем

--- Assets/_Scripts/Game/Gameplay/Apple.cs
+++ Assets/_Scripts/Game/Gameplay/Apple.cs
@@ -3,25 +3,69 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    [Header("Type of apple"), HorizontalLine(color: EColor.Red)]
-    [SerializeField]
-    private AppleType _type;
-
     [Header("Apple properties"), HorizontalLine(color: EColor.Green)]
     [SerializeField]
-    private int _cost = 1;
+    private AppleProperties[] _appleProperties;
+    [SerializeField]
+    private GameObject[] _appleStateObj;
 
-    public AppleType type => _type;
-    public int cost => _cost;
+    [SerializeField]
+    private AppleProperties _currentProperties;
 
-    public void PickThis()
+    [Header("TickHolde"), HorizontalLine(color: EColor.Gray)]
+    [SerializeField]
+    private TickCounter _tickCounter = new();
+
+    public AppleType type => _currentProperties.appleType;
+    public int cost => _currentProperties.cost;
+
+    private void Start()
+    { 
+        _currentProperties = _appleProperties[0];
+
+        _tickCounter.SetUp(OnTimeToChangeState);
+        _tickCounter.OnResume();
+
+        _tickCounter.SetTarget(_currentProperties.ticksToNextState);
+    }
+
+    public void DeactivateThis()
     {
         gameObject.SetActive(false);
-        GameEvents.appleWasPicked.Invoke(this);
+        _tickCounter.OnPause();
+        GameEvents.appleWasDeactivated.Invoke(this);
     }
+
     public void RespawnThis(Vector2 pos)
     {
+        ChangeState(0);
         transform.position = pos;
         gameObject.SetActive(true);
+        _tickCounter.OnResume();
+    }
+
+    private void OnTimeToChangeState()
+    {
+        if (_currentProperties.state == AppleState.Rotten)
+        {
+            DeactivateThis();
+            return;
+        }
+
+        ChangeState((int)_currentProperties.state + 1);
+    }
+    private void ChangeState(int stateNum)
+    {
+        _currentProperties = _appleProperties[stateNum];
+
+        _tickCounter.SetTarget(_currentProperties.ticksToNextState);
+
+        for (int i = 0; i < _appleStateObj.Length; i++)
+            _appleStateObj[i].SetActive(i == stateNum);
+    }
+
+    private void OnDestroy()
+    {
+        _tickCounter = null;
     }
 }
