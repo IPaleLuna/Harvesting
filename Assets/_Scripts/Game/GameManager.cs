15d33de488bee076b855d87719631f8f6f421e92
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-11-14 14:52:08 +0300

    Update GameManager.cs
    Обработка событий паузы

--- Assets/_Scripts/Game/GameManager.cs
+++ Assets/_Scripts/Game/GameManager.cs
@@ -1,7 +1,10 @@
 using PaleLuna.Architecture.GameComponent;
+using PaleLuna.Architecture.Loops;
 using PaleLuna.Architecture.Services;
+using PaleLuna.Patterns.State.Game;
 using PaleLuna.Timers.Implementations;
 using Services;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour, IStartable, IService
@@ -14,18 +17,24 @@ public class GameManager : MonoBehaviour, IStartable, IService
     [SerializeField]
     private int _timeToNextScene;
 
+    
+    private GameLoops _gameLoops;
+    
     private AsyncTimer _timer;
 
-    private bool _isStart = false;
-    public bool IsStarted => _isStart;
+    public bool IsStarted { get; private set; } = false;
     public AsyncTimer timer => _timer;
 
     public void OnStart()
     {
-        if (_isStart) return;
-        _isStart = true;
+        if (IsStarted) return;
+        IsStarted = true;
 
         _timer = new(_gameTimeInSeconds, OnTimeOut);
+        _gameLoops = ServiceManager.Instance.GlobalServices.Get<GameLoops>();
+        
+        GameEvents.gameOnPauseEvent.AddListener(OnPause);
+        GameEvents.exitSessionEvent.AddListener(OnExit);
 
         _timer.Start();
     }
@@ -36,6 +45,30 @@ public class GameManager : MonoBehaviour, IStartable, IService
 
         _timer = new(_timeToNextScene, LoadNextScene);
         _timer.Start();
+    }
+
+    private void OnPause(bool isPaused)
+    {
+        print(isPaused);
+        if(isPaused)
+        {
+            _timer.OnPause();
+            _gameLoops.stateHolder.ChangeState<PauseState>();
+        }
+        else
+        {
+            _timer.OnResume();
+            _gameLoops.stateHolder.ChangeState<PlayState>();
+        }
+    }
+    private void OnExit()
+    {
+        OnPause(true);
+        
+        ServiceManager.Instance.GlobalServices.Get<SceneLoaderService>().LoadSceneAsync(1, false);
+        _startScreen.FadeOut(() =>
+            ServiceManager.Instance.GlobalServices.Get<SceneLoaderService>().AllowNextScene()
+            );
     }
 
     public void LoadNextScene()
