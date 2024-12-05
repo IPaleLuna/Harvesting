d635904fef9a8a3ce078af4a64ed086f936ac1a7
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-05 20:00:34 +0300

    Update PlayerModel.cs

--- Assets/_Scripts/Game/Gameplay/PlayerScripts/PlayerMVC/PlayerModel.cs
+++ Assets/_Scripts/Game/Gameplay/PlayerScripts/PlayerMVC/PlayerModel.cs
@@ -4,6 +4,7 @@ public class PlayerModel
 {
     private int _appleAmount = 0;
     private float _speed = 0F;
+    public int playerID = -1;
 
     public int appleAmount => _appleAmount;
 
@@ -16,4 +17,23 @@ public class PlayerModel
     
     public void AddApples(int count) =>
         _appleAmount = Mathf.Max(0, _appleAmount + count);
+}
+
+public struct PlayerInfo
+{
+    public int appleAmount;
+    public float speed;
+    public int playerID;
+
+    public PlayerInfo(int appleAmount, float speed, int playerID)
+    {
+        this.appleAmount = appleAmount;
+        this.speed = speed;
+        this.playerID = playerID;
+    }
+
+    public static PlayerInfo CreatePlayerInfo(PlayerModel playerModel)
+    {
+        return new PlayerInfo(playerModel.appleAmount, playerModel.speed, playerModel.playerID);
+    }
 }
