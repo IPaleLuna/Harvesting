dd5664dd2c9dafbabe6d9903ed9954fcfd61a294
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-05 18:29:07 +0300

    PlayerController -> PlayerMovement

--- Assets/_Scripts/Game/Gameplay/PlayerScripts/PlayerMovement.cs
+++ Assets/_Scripts/Game/Gameplay/PlayerScripts/PlayerMovement.cs
@@ -1,26 +1,22 @@
 using NaughtyAttributes;
-using PaleLuna.Architecture.Loops;
-using Services;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(PlayerView))]
+public class PlayerMovement : MonoBehaviour
 {
-    [Header("Main Components"), HorizontalLine(color: EColor.Violet)]
+    [Header("Main Components"), HorizontalLine(color: EColor.Green)]
     [SerializeField]
     private Rigidbody2D _rigidbody2D;
     [SerializeField]
     private PlayerInput _playerInput;
-    [SerializeField]
-    private Player _player;
 
-    [Header("Additions components"), HorizontalLine(color: EColor.Gray)]
+    [Header("MVC components"), HorizontalLine(color: EColor.Violet)]
     [SerializeField]
-    private SpriteFlipper _spriteFlipper;
-    [SerializeField]
-    private AnimationControll _animationControl;
+    private PlayerView _view;
+    private PlayerModel _model;
 
     private PlayerInputActions _playerActions;
 
@@ -32,22 +28,24 @@ public class PlayerController : MonoBehaviour
     {
         _rigidbody2D ??= GetComponent<Rigidbody2D>();
         _playerInput ??= GetComponent<PlayerInput>();
-        _player ??= GetComponent<Player>();
+        _view ??= GetComponent<PlayerView>();
     }
 
     private void Awake()
     {
         _playerActions = new(_playerInput);
     }
-
+    
     private void OnEnable()
     {
         Subscribe();
     }
 
+    public void SetModel(PlayerModel model) => _model = model;
+
     public void Move()
     {
-        Vector2 velocity = _currentDirection * _player.characteristics.speed;
+        Vector2 velocity = _currentDirection * _model.speed;
 
         _rigidbody2D.velocity = velocity;
     }
@@ -59,16 +57,14 @@ public class PlayerController : MonoBehaviour
     public void Stop()
     {
         _rigidbody2D.velocity = Vector2.zero;
-        _animationControl.ResetAnim();
+        _view.ResetAnimations();
         Unsubscribe();
     }
 
     private void OnGetMoveInput(InputAction.CallbackContext context)
     {
         _currentDirection = context.ReadValue<Vector2>();
-
-        _spriteFlipper.OnInputDirectionChanged(_currentDirection);
-        _animationControl.OnInputDirectionChanged(_currentDirection);
+        _view.UpdateDirection(_currentDirection);
     }
 
     private void Subscribe()
