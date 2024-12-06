301cef6b0be2a09643e9a91dff7c442adb5def6f
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-06 15:45:51 +0300

    Реализация NetWorkPlayerHandler

--- Assets/_Scripts/Game/Gameplay/PlayerScripts/PlayerHandler/NetWorkPlayerHandler.cs
+++ Assets/_Scripts/Game/Gameplay/PlayerScripts/PlayerHandler/NetWorkPlayerHandler.cs
@@ -1,6 +1,77 @@
+using PaleLuna.Architecture.GameComponent;
+using PaleLuna.Architecture.Loops;
+using Services;
+using Unity.Netcode;
 using UnityEngine;
 
-public class NetWorkPlayerHandler : MonoBehaviour
+public class NetWorkPlayerHandler : NetworkBehaviour, IFixedUpdatable
 {
+    [SerializeField]
+    private PlayerController _playerController;
+    
+    private GameLoops _gameLoops;
 
+    private void OnValidate()
+    {
+        _playerController ??= GetComponent<PlayerController>();
+    }
+
+    private void Start()
+    {
+        if (!IsOwner)
+        {
+            _playerController.Remove();
+            Destroy(this);
+            return;
+        }
+        
+        _gameLoops = ServiceManager.Instance.GlobalServices.Get<GameLoops>();
+        _gameLoops.Registration(this);
+        
+        _playerController.SetUpMovement();
+        
+        GameEvents.timeOutEvent.AddListener(DisableControl);
+    }
+    
+    public void FixedFrameRun()
+    {
+        _playerController.Move();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out Apple apple))
+        {
+            _playerController.CollectApple(apple);
+            apple.DeactivateThis();
+            GameEvents.playerPickApple.Invoke(_playerController);
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        _gameLoops?.Unregistration(this);
+    }
+    #region [ Pausable implementation ]
+    public void OnPause()
+    {
+        DisableControl();
+    }
+
+    public void OnResume()
+    {
+        EnableControl();
+    }
+    #endregion
+    
+    private void EnableControl()
+    {
+        _gameLoops?.Registration(this);
+        _playerController?.IsActive(true);
+    }
+    private void DisableControl()
+    {
+        _gameLoops?.Unregistration(this);
+        _playerController?.IsActive(false);
+    }
 }
