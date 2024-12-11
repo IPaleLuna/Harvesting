99143d2db521c073da838b013941c56d2bb77a86
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-11 11:26:46 +0300

    Исправлен баг двойной подписки

--- Assets/_Scripts/Game/TickCounter.cs
+++ Assets/_Scripts/Game/TickCounter.cs
@@ -13,11 +13,15 @@ public class TickCounter : ITickUpdatable
 
     [SerializeField, Range(0, 60)]
     private int _targetTicks;
+    
+    private CounterState _counterState = CounterState.ShutDown;
 
     [SerializeField, ReadOnly]
     private int _currentTicks = 0;
 
     private UnityAction _callbackAction;
+    
+    public int currentTicks => _currentTicks;
 
     public void SetCallback(UnityAction callback)
     {
@@ -38,17 +42,26 @@ public class TickCounter : ITickUpdatable
 
     public void Pause()
     {
+        if(_counterState == CounterState.Paused) return;
+        
         _gameLoops?.Unregistration(this);
+        _counterState = CounterState.Paused;
     }
 
     public void Start()
     {
+        if (_counterState == CounterState.Runnign) return;
+        
         _gameLoops?.Registration(this);
+        _counterState = CounterState.Runnign;
     }
     public void ShutDown()
     {
+        if (_counterState == CounterState.ShutDown) return;
+        
         Pause();
         _currentTicks = 0;
+        _counterState = CounterState.ShutDown;
     }
 
     public void EveryTickRun()
@@ -72,5 +85,11 @@ public class TickCounter : ITickUpdatable
     {
         ShutDown();
     }
-    
+}
+
+public enum CounterState
+{
+    Runnign,
+    Paused,
+    ShutDown
 }
