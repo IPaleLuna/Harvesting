0f52d630aa966f212c56a35a9256d4e3646b4ba6
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-11 11:27:21 +0300

    Разделение логики

--- Assets/_Scripts/Game/Gameplay/AppleSpawner/AppleSpawner.cs
+++ Assets/_Scripts/Game/Gameplay/AppleSpawner/AppleSpawner.cs
@@ -3,146 +3,92 @@ using PaleLuna.Architecture.GameComponent;
 using System.Collections.Generic;
 using Harvesting.Collectable.Apple;
 using PaleLuna.Architecture.Loops;
+using PaleLuna.Architecture.Services;
 using Services;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public class AppleSpawner : MonoBehaviour, IStartable, IPausable
+public class AppleSpawner : IService
 {
-    #region [inspector area]
-    [FormerlySerializedAs("simpleMonoAppleHandler")]
-    [FormerlySerializedAs("simpleMonoApple")]
-    [FormerlySerializedAs("_simpleApple")]
-    [Header("Apple prefabs"), HorizontalLine(color: EColor.Red)]
-    [SerializeField]
-    private MonoAppleController simpleMonoAppleController;
-    [FormerlySerializedAs("zapMonoAppleHandler")] [FormerlySerializedAs("zapMonoApple")] [FormerlySerializedAs("_zapApple")] [SerializeField]
-    private MonoAppleController zapMonoAppleController;
-
-    [Header("Spawn properties"), HorizontalLine(color: EColor.Blue)]
-
-    [SerializeField, Range(0f, 1f)]
-    private float _percentageTreesSpawn = 0.5f;
-
-    [SerializeField, Range(0f, 1f)]
-    private float _zapAppleSpawnChance = 0.3F;
-
-    [SerializeField, Range(0, 300)]
-    private int _maxApplesOnLevel;
-
-    [Space, SerializeField, Required]
-    private Transform _paretnForSimpleApples;
-    [Space, SerializeField, Required]
-    private Transform _paretnForZapApples;
-
-    [Space, SerializeField]
-    private TickCounter _tickHolder = new();
-
-    [Header("List of tree"), HorizontalLine(color: EColor.Orange)]
-    [SerializeReference]
-    private List<AppleTree> _appleTrees;
-    #endregion
-
-    private ObjectPool<MonoAppleController> _simpeApplePool;
-    private ObjectPool<MonoAppleController> _zapApplePool;
+    private readonly AppleSpawnerModel _model;
+    
+    private ObjectPool<AppleHandler> _simpleApplePool;
+    private ObjectPool<AppleHandler> _zapApplePool;
 
     private int _treeStep;
     private int _totalSteps;
     
     public bool IsStarted { get; private set; } = false;
 
-    public void OnStart()
+    public AppleSpawner(AppleSpawnerModel model)
     {
-        if (IsStarted) return;
-        IsStarted = true;
-
+        _model = model;
+        
         PoolInit();
         TreeInit();
 
-        _tickHolder.SetUp(OnTimeToSpawn);
-
-        GameEvents.appleWasDeactivated.AddListener(ReturnAppleInPool);
-
-        OnTimeToSpawn();
-        
-        ServiceManager.Instance
-            .GlobalServices.Get<GameLoops>()
-            .pausablesHolder.Registration(this);
+        ServiceManager.Instance.LocalServices.Registarion(this);
     }
-
-    private void PoolInit()
-    {
-        _simpeApplePool = new(_maxApplesOnLevel, simpleMonoAppleController, _paretnForSimpleApples);
-        _zapApplePool = new((int)(_maxApplesOnLevel * _zapAppleSpawnChance), zapMonoAppleController, _paretnForZapApples);
-
-        _simpeApplePool.Generate();
-        _zapApplePool.Generate();
-    }
-    private void TreeInit()
-    {
-        _appleTrees = new(GetComponentsInChildren<AppleTree>());
-
-        _treeStep = Mathf.RoundToInt(1 / _percentageTreesSpawn);
-        _totalSteps = Mathf.RoundToInt(_appleTrees.Count * _percentageTreesSpawn);
-    }
-
-    private void OnTimeToSpawn()
+    
+    public void Spawn()
     {
         for (int i = 0; i < _totalSteps; i++)
         {
             AppleTree tree = PeekRandomTree(i);
 
-            if (Random.Range(0f, 1f) < _zapAppleSpawnChance)
+            if (Random.Range(0f, 1f) < _model.zapAppleSpawnChance)
                 tree.PlaceApple(_zapApplePool);
             else
-                tree.PlaceApple(_simpeApplePool);
+                tree.PlaceApple(_simpleApplePool);
         }
+    }
+
+    public void ReturnToPool(AppleHandler appleHandler)
+    {
+        switch (appleHandler.appleController.type)
+        {
+            case AppleType.SimpleApple:
+                _simpleApplePool.Enqueue(appleHandler);
+                break;
+            case AppleType.ZapApple:
+                _zapApplePool.Enqueue(appleHandler);
+                break;
+        }
+    }
+    
+    private void PoolInit()
+    {
+        _simpleApplePool = new(
+            _model.maxApplesOnLevel,
+            _model.simpleApplePrefab,
+            _model.parentForSimpleApples
+            );
+        
+        _zapApplePool = new(
+            (int)(_model.maxApplesOnLevel * _model.zapAppleSpawnChance), 
+            _model.zapApplePrefab, 
+            _model.parentForZapApples
+            );
+
+        _simpleApplePool.Generate();
+        _zapApplePool.Generate();
+    }
+    private void TreeInit()
+    {
+        _treeStep = Mathf.RoundToInt(1 / _model.percentageTreesSpawn);
+        _totalSteps = Mathf.RoundToInt(_model.appleTrees.Count * _model.percentageTreesSpawn);
+        
+        Debug.Log($"steps: {_treeStep}/{_totalSteps}");
     }
 
     private AppleTree PeekRandomTree(int currentStep)
     {
         int start = currentStep * _treeStep;
-        int end = currentStep == _totalSteps - 1 ? _appleTrees.Count : start + (_treeStep - 1);
+        int end = currentStep == _totalSteps - 1 ? _model.appleTrees.Count : start + (_treeStep - 1);
 
         int randomIndex = Random.Range(start, end);
 
-        return _appleTrees[randomIndex];
+        return _model.appleTrees[randomIndex];
     }
-
-    private void ReturnAppleInPool(MonoAppleController monoAppleController)
-    {
-        switch (monoAppleController.type)
-        {
-            case AppleType.SimpleApple:
-                _simpeApplePool.Enqueue(monoAppleController);
-                break;
-            case AppleType.ZapApple:
-                _zapApplePool.Enqueue(monoAppleController);
-                break;
-        }
-    }
-
-    private void OnDestroy()
-    {
-        _tickHolder.ShutDown();
-        ServiceManager.Instance
-            ?.GlobalServices?.Get<GameLoops>()
-            ?.pausablesHolder?.Unregistration(this);
-        
-    }
-
-    #region [ Pausable implementation ]
-
-    public void OnPause()
-    {
-        _tickHolder.Pause();
-    }
-
-    public void OnResume()
-    {
-        _tickHolder.Start();
-    }
-
-    #endregion
 }
---
+++ Assets/_Scripts/Game/Gameplay/AppleSpawner/MonoAppleSpawnerHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using PaleLuna.Architecture.GameComponent;
+using PaleLuna.Architecture.Loops;
+using Services;
+using UnityEngine;
+
+public class MonoAppleSpawnerHandler : MonoBehaviour, IPausable
+{
+    [SerializeField]
+    private AppleSpawnerModel _model;
+    
+    [Space, SerializeField]
+    private TickCounter _tickHolder = new();
+    
+    private AppleSpawner _spawner;
+
+    private void Start()
+    {
+        _model.CollectTrees();
+        _spawner = new AppleSpawner(_model);
+        
+        _tickHolder.SetUp(OnTick);
+        
+        OnTick();
+        
+        ServiceManager.Instance
+            .GlobalServices.Get<GameLoops>()
+            .pausablesHolder.Registration(this);
+    }
+
+    private void OnTick()
+    {
+        _spawner.Spawn();
+    }
+
+    #region [ Pausable implementation ]
+
+    public void OnPause()
+    {
+        _tickHolder.Pause();
+    }
+
+    public void OnResume()
+    {
+        _tickHolder.Start();
+    }
+
+    #endregion
+}
