224d8419f4aad9353e4e1e3b02faf081b4f54418
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-12 16:27:28 +0300

    Update MonoTimerController.cs

--- Assets/_Scripts/Game/Gameplay/Timer/Controllers/MonoTimerController.cs
+++ Assets/_Scripts/Game/Gameplay/Timer/Controllers/MonoTimerController.cs
@@ -1,68 +1,56 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
-using Harvesting.Game.GameTimer;
 using UnityEngine;
 
-public class MonoTimerController : MonoBehaviour, ITimeController
+namespace Harvesting.Game.GameTimer
 {
-    [SerializeField]
-    private int _gameTimeInSeconds;
-    [SerializeField]
-    private int _afterGameTimeInSeconds;
+    public class MonoTimerController : MonoBehaviour, ITimeController
+    {
+        [SerializeField]
+        private int _gameTimeInSeconds;
+        [SerializeField]
+        private int _afterGameTimeInSeconds;
+        
+        private GameTimer _gameTimer;
     
-    private GameTimer _gameTimer;
-
-    private bool isInit = false;
-    private bool isAwait = false;
-
-
-    public void Awake()
-    {
-        _gameTimer = new GameTimer(_gameTimeInSeconds, _afterGameTimeInSeconds);
-
-        _gameTimer.onGameTimerFinished += () => GlobalTimeEvents.onGameTimerFinished?.Invoke();
-        _gameTimer.onAfterGameTimerFinished += () => GlobalTimeEvents.onAfterGameTimerFinished?.Invoke();
-        _gameTimer.onTick += (time) => GlobalTimeEvents.onTick?.Invoke(time);
+        public bool isInit { get; private set; }= false;
         
-        isInit = true;
-    }
-
-    public void Init()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void StartGameTimer()
-    {
-        if (isInit) _gameTimer.StartGameTimer(); 
-        _ = DelayedStart();
-    }
-
-    public void StartAfterGameTimer()
-    {
-        _gameTimer.StartAfterGameTimer();
-    }
-
-    public void Pause()
-    {
-        _gameTimer.PauseGameTimer();
-    }
-
-    public void Resume()
-    {
-        _gameTimer.ResumeGameTimer();
-    }
-
-    private async UniTaskVoid DelayedStart()
-    {
-        if(isAwait) return;
-        isAwait = true;
-        
-        while (!isInit)
-            await UniTask.Yield();
-        
-        StartGameTimer();        
+        [SerializeField]
+        private bool _autoRun = true;
+    
+    
+        public void Awake()
+        {
+            _gameTimer = new GameTimer(_gameTimeInSeconds, _afterGameTimeInSeconds);
+    
+            _gameTimer.onGameTimerFinished += () => GlobalTimeEvents.onGameTimerFinished?.Invoke();
+            _gameTimer.onAfterGameTimerFinished += () => GlobalTimeEvents.onAfterGameTimerFinished?.Invoke();
+            _gameTimer.onTick += (time) => GlobalTimeEvents.onTick?.Invoke(time);
+            
+            isInit = true;
+            
+            if(_autoRun) StartGameTimer();
+        }
+    
+        public void StartGameTimer()
+        {
+            _gameTimer.StartGameTimer(); 
+        }
+    
+        public void StartAfterGameTimer()
+        {
+            _gameTimer.StartAfterGameTimer();
+        }
+    
+        public void Pause()
+        {
+            _gameTimer.PauseGameTimer();
+        }
+    
+        public void Resume()
+        {
+            _gameTimer.ResumeGameTimer();
+        }
     }
 }
+
+
