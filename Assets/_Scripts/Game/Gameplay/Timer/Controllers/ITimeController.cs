1af60fac90e983127cd69acabd7b38aded8e10aa
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-18 22:34:06 +0300

    Update ITimeController.cs

--- Assets/_Scripts/Game/Gameplay/Timer/Controllers/ITimeController.cs
+++ Assets/_Scripts/Game/Gameplay/Timer/Controllers/ITimeController.cs
@@ -2,8 +2,6 @@ namespace Harvesting.Game.GameTimer
 {
     public interface ITimeController
     {
-        public bool isInit { get; }
-    
         public void StartGameTimer();
         public void StartAfterGameTimer();
     
