934b186f62c9bcdc2926ca0e0da7187a5f15118b
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-19 17:52:16 +0300

    Update StartGameTigger.cs

--- Assets/_Scripts/Game/Gameplay/Objects/StartGameTigger.cs
+++ Assets/_Scripts/Game/Gameplay/Objects/StartGameTigger.cs
@@ -1,25 +1,31 @@
-using System;
-using Harvesting.PlayerHandler;
 using PaleLuna.Network;
-using PaleLuna.Timers.Implementations;
+using Services;
+using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.Serialization;
+using NetworkSceneManager = Harvesting.Networking.NetworkSceneManager;
 
 
 public class StartGameTigger : NetworkLunaBehaviour
 {
-    private int playerCounter = 0;
+    [SerializeField]
+    private CountdownView _countdownView;
 
-    [FormerlySerializedAs("timer")] [SerializeField]
+    [SerializeField]
+    private bool _isDebug = false;
+    
+    [SerializeField]
+    private BackstageScreen _backstageScreen;
+    
+    [SerializeField]
     private TickCounter _tickCounter = new TickCounter();
+    
+    
+    private int playerCounter = 0;
+    
     
     public override void InitNetworkBehaviour()
     {
-        if (!IsServer)
-        {
-            Destroy(this);
-            return;
-        }
+        if (!IsServer) return;
         
         _tickCounter.SetUp(LoadGameScene);
         _tickCounter.onEveryTickAction += SendTimer;
@@ -27,17 +33,18 @@ public class StartGameTigger : NetworkLunaBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<NetworkPlayerView>() == null) return;
+        if (other.GetComponent<NetworkPlayerView>() == null || !IsServer) return;
         
         playerCounter++;
 
-        if (playerCounter > 1 && playerCounter == NetworkManager.ConnectedClients.Count)
+        if ((_isDebug || playerCounter > 1) && playerCounter == NetworkManager.ConnectedClients.Count)
             _tickCounter.Start();
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.GetComponent<NetworkPlayerView>() == null) return;
+        if (other.GetComponent<NetworkPlayerView>() == null || !IsServer) return;
+        
         if(playerCounter == NetworkManager.ConnectedClients.Count)
             _tickCounter.ShutDown();
         
@@ -46,14 +53,32 @@ public class StartGameTigger : NetworkLunaBehaviour
 
     private void SendTimer()
     {
-        print($"Wait for... {_tickCounter.targetTicks - _tickCounter.currentTicks}");
+        _countdownView
+            .SetCountdownTextClientRpc(NumToStringBuffer
+                .GetIntToStringHash(
+                    _tickCounter.targetTicks - _tickCounter.currentTicks
+                    )
+            );
     }
 
     private void LoadGameScene()
     {
         _tickCounter.ShutDown();
-        print("Loading Game Scene");
+        SendBackStageClientRpc();
         
+        if (IsServer)
+        {
+            _backstageScreen.FadeOut(() => 
+                ServiceManager.Instance.
+                    GlobalServices.Get<NetworkSceneManager>()
+                    .SwitchScene("NetGameScene"));
+        }
+    }
+
+    [ClientRpc]
+    private void SendBackStageClientRpc()
+    {
+        _backstageScreen.FadeOut();
     }
     
     
