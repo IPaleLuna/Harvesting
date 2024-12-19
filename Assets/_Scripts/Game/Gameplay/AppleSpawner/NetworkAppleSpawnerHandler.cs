583d9f0215a9fe03ce67ed6dec35b40f66ba0b2c
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-12-19 23:12:35 +0300

    Update NetworkAppleSpawnerHandler.cs

--- Assets/_Scripts/Game/Gameplay/AppleSpawner/NetworkAppleSpawnerHandler.cs
+++ Assets/_Scripts/Game/Gameplay/AppleSpawner/NetworkAppleSpawnerHandler.cs
@@ -43,4 +43,10 @@ public class NetworkAppleSpawnerHandler : NetworkLunaBehaviour
     {
         _spawner.Spawn();
     }
+
+    public override void OnDestroy()
+    {
+        _tickHolder.ShutDown();
+        base.OnDestroy();
+    }
 }
